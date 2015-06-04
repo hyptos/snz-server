224ed75d14ba34b1bd8878621bf3431a2e2db0e3
Avzgui <ri_antoine@yahoo.fr>
2015-06-04 14:22:37 +0200

    modif Environment::getVisualStimulus

--- C++/Application/Model/environment.cpp
+++ C++/Application/Model/environment.cpp
@@ -89,5 +89,32 @@ VisualStimulus Environment::getVisualStimulus(double x, double z, double distanc
         }
     }
 
+
+    for(std::list<RABody*>::iterator it = m_agents.begin(); it != m_agents.end(); it++){
+
+        (*it)->lock();
+        std::pair<double,double> pos;
+        pos.first = (*it)->getX();
+        pos.second = (*it)->getZ();
+        (*it)->unlock();
+
+        //On commence par regarder ceux dans la bonne distance
+        double dist = std::sqrt(std::pow(pos.first - x, 2.0) + std::pow(pos.second - z, 2.0));
+
+        if(dist <= distance){
+
+            //On calcul son angle par rapport à la position (x, z)
+            double x2 = (pos.first - x) / dist;
+            double z2 = (pos.second - x) / dist;
+            double gamma = atan(x2 / z2);
+            if(z2 < 0)
+                gamma += M_PI;
+
+            //On regarde si l'entité se trouve dans le cone
+            if(gamma > beta - alpha || gamma < beta + alpha)
+                stimulus.pushData(EntityType::PLAYER, pos);
+        }
+    }
+
     return stimulus;
 }
